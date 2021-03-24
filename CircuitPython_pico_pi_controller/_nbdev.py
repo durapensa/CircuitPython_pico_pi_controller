a333c2751fa037993d278da43cb0344818258478
Durapensa <durapensa@gmail.com>
2021-03-23 22:05:57 -0400

    uschedule works (see gist), on to classes for schedule & reports

--- CircuitPython_pico_pi_controller/_nbdev.py
+++ CircuitPython_pico_pi_controller/_nbdev.py
@@ -3,6 +3,7 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"IDENTITY": "00_core.ipynb",
+         "BUFCLR": "00_core.ipynb",
          "CLR": "00_core.ipynb",
          "IDF": "00_core.ipynb",
          "HOS": "00_core.ipynb",
@@ -11,10 +12,12 @@ index = {"IDENTITY": "00_core.ipynb",
          "LOD": "00_core.ipynb",
          "TZO": "00_core.ipynb",
          "PEN": "00_core.ipynb",
+         "UNDevice": "00_core.ipynb",
          "PPDevice": "00_core.ipynb",
          "PPController": "00_core.ipynb"}
 
-modules = ["core.py"]
+modules = ["core.py",
+           "schedule.py"]
 
 doc_url = "https://durapensa.github.io/CircuitPython_pico_pi_controller/"
 
